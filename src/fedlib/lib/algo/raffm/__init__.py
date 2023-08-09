b78306b6795073ace5510e0c038873760a77a9ae
Sixing Yu <yusx@iastate.edu>
2023-08-08 20:48:28 -0500

    raffm

--- src/fedlib/lib/algo/raffm/__init__.py
+++ src/fedlib/lib/algo/raffm/__init__.py
@@ -4,33 +4,31 @@ import random
 import numpy as np
 import copy
 import torch
+import time
 def calculate_trainable_params(masked_model):
-    """
-    Calculate the number of trainable model parameters
-    
+    """calculate the number of trainable parameters in the model
     Args:
-        model: the model to be evaluated
-    
+        masked_model: the model to be evaluated
     Returns:
-        total_trainable_params: the number of trainable model parameters (in millions)
-        total_params: the number of all model parameters (in millions)
-        percentage: the percentage of trainable model parameters
+        total_trainable_params: the number of trainable parameters in the model
+        total_params: the number of parameters in the model
+        percentage: the percentage of trainable parameters in the model
     """
-
+    
     millions = 1000000
     total_trainable_params = 0
     total_params = 0
     for name, module in masked_model.named_modules():
         if hasattr(module, 'weight') and isinstance(module.weight, Parameter):
             original_channels = module.weight.size(0)
-            mask = getattr(module, 'weight_mask', torch.ones(original_channels).to(module.weight.device))  # Retrieve the mask from the module
-            trainable_channels = torch.sum(mask).item()
-            total_params_in_module = trainable_channels * torch.prod(torch.tensor(module.weight.size()[1:])).item()
-            total_trainable_params += total_params_in_module
+            mask = getattr(module, 'weight_mask', torch.ones_like(module.weight).to(module.weight.device))  # Retrieve the mask from the module
+            
+            # Calculate the number of trainable parameters by summing the mask
+            trainable_params_in_module = torch.sum(mask).item()
+            total_trainable_params += trainable_params_in_module
             total_params += torch.prod(torch.tensor(module.weight.size())).item()
 
     return total_trainable_params/millions, total_params/millions, total_trainable_params/total_params
-
 def reordering_weights(model):
     """
     Reorder the weights of the model based on the matrix norm of each channel.
@@ -129,7 +127,7 @@ def subnetwork_extraction(model, target_model_params_size):
         else:
             return subnetwork
         
-def gradient_masking_extraction(model, target_model_params_size):
+def gradient_masking_extraction(model, target_model_params_size=None):
     """
     Extract a subnetwork from the original network based on the target model parameters size
     Args:
@@ -138,19 +136,23 @@ def gradient_masking_extraction(model, target_model_params_size):
     Returns:
         subnetwork: the extracted subnetwork
     """
-
+    millions = 1000000
     possible_channels = [256,384,512,640,728,1024,2048]
     dense_channels = [512,728,1024,2048,3072]
     while True:
+        total_trainable_params = 0
+        total_params = 0
+        random.seed(time.time())
         subnetwork = copy.deepcopy(model)
         previous_channels = 0
         for name, module in subnetwork.named_modules():
 
                     
             if hasattr(module, 'weight') and isinstance(module.weight, Parameter) and module.weight.requires_grad:
+                total_params += torch.prod(torch.tensor(module.weight.size())).item()
                 # print(name, module.weight.size())
                 if "encoder" not in name:
-                    
+                    total_trainable_params += torch.prod(torch.tensor(module.weight.size())).item()
                     continue
                 original_channels = module.weight.size(0)
                 # print(name, "subnet before slicing: ",module.weight.size())
@@ -159,78 +161,69 @@ def gradient_masking_extraction(model, target_model_params_size):
                     
                     sampled_channels = random.choice(possible_channels)
                     sampled_channels = min(sampled_channels, original_channels)
-                    '''
-                    if previous_channels != 0:
-                        new_weight = new_weight[:, :previous_channels]
-                        new_bias = new_bias[:previous_channels]
-
-                    new_weight = module.weight.data[:sampled_channels]
-                    new_bias = module.bias.data[:sampled_channels]
-                    module.weight = Parameter(new_weight)
-                    module.bias = Parameter(new_bias)
-                    '''
-
-                    # Create a mask that keeps the first new_channels' gradients and masks out the others
-                    mask = torch.cat([torch.ones(sampled_channels), torch.zeros(original_channels - sampled_channels)]).to(module.weight.device)
-                    module.weight_mask = mask  # Store the mask within the module
+                    mask = torch.cat([torch.ones(sampled_channels), torch.zeros(original_channels - sampled_channels)]).to('cuda')
                     module.weight.register_hook(lambda grad: grad * mask if grad is not None else None)
-
+                    trainable_params_in_module = torch.sum(mask).item()
+                    total_trainable_params += trainable_params_in_module
                 elif name.endswith('.key'):
                     # key is always after by query and should match query, so here we dont need to sample again
-                    mask = torch.cat([torch.zeros(sampled_channels), torch.ones(original_channels - sampled_channels)]).to(module.weight.device)
-                    module.weight_mask = mask  # Store the mask within the module
+                    # mask = torch.cat([torch.zeros(sampled_channels), torch.ones(original_channels - sampled_channels)]).to(module.weight.device)
+                    mask = torch.cat([torch.zeros(sampled_channels), torch.ones(original_channels - sampled_channels)]).to('cuda')
+                    # module.weight_mask = mask  # Store the mask within the module
                     module.weight.register_hook(lambda grad: grad * mask if grad is not None else None)
-
-                    '''
-                    new_weight = module.weight.data[:sampled_channels]
-                    new_bias = module.bias.data[:sampled_channels]
-                    if previous_channels != 0:
-                        new_weight = new_weight[:, :previous_channels]
-                        new_bias = new_bias[:previous_channels]
-
-                    module.weight = Parameter(new_weight)
-                    module.bias = Parameter(new_bias)
-                    '''
+                    trainable_params_in_module = torch.sum(mask).item()
+                    total_trainable_params += trainable_params_in_module
+                    
+                    
                 elif name.endswith('.value'):
                     
-                    mask = torch.cat([torch.zeros(sampled_channels), torch.ones(original_channels - sampled_channels)]).to(module.weight.device)
+                    # mask = torch.cat([torch.zeros(sampled_channels), torch.ones(original_channels - sampled_channels)]).to(module.weight.device)
+                    mask = torch.cat([torch.zeros(sampled_channels), torch.ones(original_channels - sampled_channels)]).to('cuda')
                     module.weight_mask = mask  # Store the mask within the module
                     module.weight.register_hook(lambda grad: grad * mask if grad is not None else None)
-
-                    '''
-                    new_weight = module.weight.data[:sampled_channels]
-                    new_bias = module.bias.data[:sampled_channels]
-                    if previous_channels != 0:
-                        new_weight = new_weight[:, :previous_channels]
-                        new_bias = new_bias[:previous_channels]
-                
-
-                    module.weight = Parameter(new_weight)
-                    module.bias = Parameter(new_bias)
+                    trainable_params_in_module = torch.sum(mask).item()
+                    total_trainable_params += trainable_params_in_module
                     
-                    previous_channels = num_channels
-                    '''
+
                 elif name.endswith('dense'):
                     dense_sampled_channels = random.choice(dense_channels)
                     dense_sampled_channels = min(dense_sampled_channels, original_channels)
-                    mask = torch.cat([torch.zeros(dense_sampled_channels), torch.ones(original_channels - dense_sampled_channels)]).to(module.weight.device)
-                    module.weight_mask = mask  # Store the mask within the module
-                    module.weight.register_hook(lambda grad: grad * mask if grad is not None else None)
                     
-                    '''
-                    new_weight = module.weight.data[:sampled_channels]
-                    new_bias = module.bias.data[:sampled_channels]
-                    module.weight = Parameter(new_weight)
-                    module.bias = Parameter(new_bias)
-                    '''
+                    def mask_grad(grad):
+                        if grad is None:
+                            return None
+                        # Create a mask with the same shape as the gradient
+                        mask = torch.zeros_like(grad).to(grad.device)
+                        # Set the first dense_sampled_channels rows to ones
+                        mask.narrow(0, 0, dense_sampled_channels).fill_(1)
+                        return grad * mask
 
-                
-                
+                    module.weight.register_hook(mask_grad)
+                    # You can still store the mask in the module if needed, using the original weight shape
+                    mask_for_module = torch.zeros_like(module.weight).to('cuda')
+                    mask_for_module[:dense_sampled_channels, :] = 1
+                    # module.weight_mask = mask_for_module
+                    trainable_params_in_module = torch.sum(mask_for_module).item()
+                    total_trainable_params += trainable_params_in_module
+        
+        total_trainable_params = total_trainable_params / millions
+        total_params = total_params / millions
+        percentage = total_trainable_params / total_params
+        
+        if target_model_params_size is None:
+            return subnetwork, total_trainable_params, total_params, percentage
+        
 
         # Evaluate current network FLOPs
-        trainable_model_params, all_model_params, percentage = calculate_trainable_params(subnetwork)
-
-        if trainable_model_params > target_model_params_size:
-            continue
+        # trainable_model_params, all_model_params, percentage = calculate_trainable_params(subnetwork)
+        
+        if target_model_params_size>0 and target_model_params_size<1:
+            if percentage > target_model_params_size:
+                continue
+            else:
+                return subnetwork, total_trainable_params, total_params, percentage
         else:
-            return subnetwork
+            if total_trainable_params > target_model_params_size:
+                continue
+            else:
+                return subnetwork, total_trainable_params, total_params, percentage
